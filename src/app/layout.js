06c27def90a4400b9b936d3981009e6cd79a7bd7
ezzatabdeen.dev <ezzatabdeen.dev@gmail.com>
2025-07-18 16:35:28 -0400

    add manifest and edit layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+/*
 import "./globals.css";
 import { Header, Footer } from "@/components/index";
 
@@ -17,6 +18,40 @@ export const metadata = {
 export default function LocaleLayout({ children }) {
   return (
     <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
+ */
+
+import "./globals.css";
+import { Header, Footer } from "@/components/index";
+
+export const metadata = {
+  title: "Ezzat Abdeen",
+  description:
+    "Ezzat Abdeen is a seasoned full-stack web developer and entrepreneur with a strong background in Management Information Systems. With a BSc in MIS, Ezzat combines technical expertise with entrepreneurial acumen to deliver cutting-edge web applications and solutions. His skills encompass a wide range of web development technologies and entrepreneurial strategies, enabling him to lead projects from concept to completion.",
+};
+
+export default function LocaleLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* Favicon Icons */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Optional Meta for Theme */}
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body>
         <Header />
         {children}
