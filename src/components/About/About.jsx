73e233cbe30ed5f21ea7ad8970495e0f654604ff
Ezzat Abdeen <ezzatabdeen.dev@gmail.com>
2026-03-14 13:03:09 +0200

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ export const About = ({ data }) => {
     <div className={`container ${styles.AboutContainer}`}>
       <div className={styles.AboutoWrapper}>
         <section id="about" className={styles.AboutItem}>
-          <h2 className="text-4 line-4 sectionTitlBbefore">{data.title}</h2>
+          <h2 className="text-5 line-5 sectionTitlBbefore">{data.title}</h2>
           <p className="text-3 line-3">{data.text}</p>
         </section>
       </div>
