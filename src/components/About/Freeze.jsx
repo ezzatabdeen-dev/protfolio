3462c1cccfb5771fcfbf6684174fe5600dd83924
Ezzat Abdeen <ezzatabdeen.dev@gmail.com>
2026-03-17 11:05:07 +0200

    Update Freeze.jsx

--- src/components/About/Freeze.jsx
+++ src/components/About/Freeze.jsx
@@ -35,8 +35,7 @@ export const Freeze = () => {
     <div className={styles.overlay}>
       <h1 className={styles.errorText} title="Resource was permanently removed.">
         Error 410 <br />
-        17.5.2019 <br />
-        5.3.2026
+        I was always here, trying to create something beautiful.. but I finally realized I don’t belong in this crowd. The last light has gone out. Don't look for me.
       </h1>
     </div>
   );
